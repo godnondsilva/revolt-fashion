0898fc0c42fed6302abc665c66e4bc049cb8f99d
Godnon Dsilva <godnondsilva@gmail.com>
2022-04-20 20:16:20 +0530

    Add redux-persist library to persist cart and categories data

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,21 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import { rootReducer } from './rootReducer';
 
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [logger];
 const composedEnhancer = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancer);
+export const store = createStore(persistedReducer, undefined, composedEnhancer);
+
+export const persistor = persistStore(store);
