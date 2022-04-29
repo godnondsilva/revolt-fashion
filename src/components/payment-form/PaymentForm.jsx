3c58dd11198c33d7073f7b3c4fa6afd2cc9a5818
Godnon Dsilva <godnondsilva@gmail.com>
2022-04-29 22:30:45 +0530

    Update pay now button to call the createPaymentIntent serverless function

--- src/components/payment-form/PaymentForm.jsx
+++ src/components/payment-form/PaymentForm.jsx
@@ -7,20 +7,49 @@ const PaymentForm = () => {
 	const stripe = useStripe();
 	const elements = useElements();
 
-	const paymentHandler = (e) => {
+	const paymentHandler = async (e) => {
 		e.preventDefault();
 
 		if (!stripe || !elements) return;
+
+		const response = await fetch('/.netlify/functions/createPaymentIntent', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ amount: 100 }),
+		}).then((response) => response.json());
+
+		const {
+			paymentIntent: { client_secret },
+		} = response;
+
+		const paymentResult = await stripe.confirmCardPayment(client_secret, {
+			payment_method: {
+				card: elements.getElement(CardElement),
+				billing_details: {
+					name: 'Godnon Dsilva',
+				},
+			},
+		});
+
+		console.log(paymentResult);
+
+		if (paymentResult.error) {
+			alert(paymentResult.error.message);
+		} else {
+			if (paymentResult.paymentIntent.status === 'succeeded') {
+				alert('Payment Successful!');
+			}
+		}
 	};
 
 	return (
 		<PaymentFormContainer>
-			<FormContainer>
+			<FormContainer onSubmit={paymentHandler}>
 				<h2>Credit Card Payment:</h2>
 				<CardElement />
-				<Button buttonType={BUTTON_TYPES.inverted} onClick={paymentHandler}>
-					Pay now
-				</Button>
+				<Button buttonType={BUTTON_TYPES.inverted}>Pay now</Button>
 			</FormContainer>
 		</PaymentFormContainer>
 	);
